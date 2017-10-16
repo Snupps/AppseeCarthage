92d5bcd903fe03faa7ec2f8463b2f11a2f1d9b18
Peyman Khanjan <p.khanjan@gmail.com>
2017-10-16 12:12:44 +0100

    AppSee Carthage Wrapper.

---
+++ AppseeCarthage/AppseeCarthage/AppseeCarthage.h
@@ -0,0 +1,12 @@
+//
+//  AppseeCarthage.h
+//  Appsee
+//
+
+#import <Appsee/Appsee.h>
+
+FOUNDATION_EXPORT double AppseeVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char AppseeVersionString[];
+
+
