d900ec62a306c70e1b050c0bb1cad546bda9434f
Peyman Khanjan <p.khanjan@gmail.com>
2018-11-30 15:27:53 +0000

    Appsee v2.4.6.

--- AppseeCarthage/AppseeCarthage/Library/Headers/Appsee.h
+++ AppseeCarthage/AppseeCarthage/Library/Headers/Appsee.h
@@ -1,6 +1,6 @@
 //
 //  Appsee.h
-//  Appsee v2.4.5
+//  Appsee v2.4.6
 //
 //  Copyright (c) 2018 Shift 6 Ltd. All rights reserved.
 //
@@ -211,7 +211,6 @@ This method should be usually called right after the start: method.
  * @param delegate an instance of AppseeDelegate
  */
 +(void)setDelegate:(id<AppseeDelegate>)delegate;
-
 
 @end
 
